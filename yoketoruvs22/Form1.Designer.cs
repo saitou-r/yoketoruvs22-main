77cd1f4f511eb6d9ec7ae1fc29fb91c75f891dba
A061-09 <tnk.dat.class@gmail.com>
2022-09-07 09:17:55 +0900

    Initial commit

---
+++ yoketoruvs22/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace yoketoruvs22
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.copyrightLabel = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.leftLabel = new System.Windows.Forms.Label();
+            this.hiLabel = new System.Windows.Forms.Label();
+            this.gameOverLabel = new System.Windows.Forms.Label();
+            this.clearLabel = new System.Windows.Forms.Label();
+            this.titleButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tempLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("源ノ角ゴシック Code JP R", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.titleLabel.ForeColor = System.Drawing.Color.Black;
+            this.titleLabel.Location = new System.Drawing.Point(161, 59);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(296, 93);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "ミツケル";
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Yu Gothic UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.startButton.Location = new System.Drawing.Point(202, 220);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(221, 75);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "スタート!!";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // copyrightLabel
+            // 
+            this.copyrightLabel.AutoSize = true;
+            this.copyrightLabel.Font = new System.Drawing.Font("Yu Gothic UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.copyrightLabel.Location = new System.Drawing.Point(222, 322);
+            this.copyrightLabel.Name = "copyrightLabel";
+            this.copyrightLabel.Size = new System.Drawing.Size(189, 30);
+            this.copyrightLabel.TabIndex = 2;
+            this.copyrightLabel.Text = "Copyright © 2022";
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Consolas", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.timeLabel.Location = new System.Drawing.Point(12, 10);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(108, 26);
+            this.timeLabel.TabIndex = 3;
+            this.timeLabel.Text = "Time 120";
+            // 
+            // leftLabel
+            // 
+            this.leftLabel.AutoSize = true;
+            this.leftLabel.Font = new System.Drawing.Font("Consolas", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.leftLabel.Location = new System.Drawing.Point(550, 10);
+            this.leftLabel.Name = "leftLabel";
+            this.leftLabel.Size = new System.Drawing.Size(63, 26);
+            this.leftLabel.TabIndex = 4;
+            this.leftLabel.Text = "◆：01";
+            // 
+            // hiLabel
+            // 
+            this.hiLabel.AutoSize = true;
+            this.hiLabel.Font = new System.Drawing.Font("Consolas", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.hiLabel.Location = new System.Drawing.Point(222, 158);
+            this.hiLabel.Name = "hiLabel";
+            this.hiLabel.Size = new System.Drawing.Size(0, 26);
+            this.hiLabel.TabIndex = 5;
+            // 
+            // gameOverLabel
+            // 
+            this.gameOverLabel.AutoSize = true;
+            this.gameOverLabel.Font = new System.Drawing.Font("Broadway", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.gameOverLabel.ForeColor = System.Drawing.Color.Blue;
+            this.gameOverLabel.Location = new System.Drawing.Point(127, 150);
+            this.gameOverLabel.Name = "gameOverLabel";
+            this.gameOverLabel.Size = new System.Drawing.Size(371, 61);
+            this.gameOverLabel.TabIndex = 6;
+            this.gameOverLabel.Text = "GAME OVER";
+            // 
+            // clearLabel
+            // 
+            this.clearLabel.AutoSize = true;
+            this.clearLabel.Font = new System.Drawing.Font("Broadway", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.clearLabel.ForeColor = System.Drawing.Color.Red;
+            this.clearLabel.Location = new System.Drawing.Point(181, 84);
+            this.clearLabel.Name = "clearLabel";
+            this.clearLabel.Size = new System.Drawing.Size(262, 61);
+            this.clearLabel.TabIndex = 7;
+            this.clearLabel.Text = "CLEAR!!";
+            // 
+            // titleButton
+            // 
+            this.titleButton.Font = new System.Drawing.Font("Yu Gothic UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.titleButton.Location = new System.Drawing.Point(202, 221);
+            this.titleButton.Name = "titleButton";
+            this.titleButton.Size = new System.Drawing.Size(221, 75);
+            this.titleButton.TabIndex = 8;
+            this.titleButton.Text = "タイトルへ";
+            this.titleButton.UseVisualStyleBackColor = true;
+            this.titleButton.Click += new System.EventHandler(this.titleButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tempLabel
+            // 
+            this.tempLabel.Font = new System.Drawing.Font("Yu Gothic UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tempLabel.Location = new System.Drawing.Point(35, 96);
+            this.tempLabel.Name = "tempLabel";
+            this.tempLabel.Size = new System.Drawing.Size(25, 27);
+            this.tempLabel.TabIndex = 9;
+            this.tempLabel.Text = "◆";
+            this.tempLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(624, 361);
+            this.Controls.Add(this.tempLabel);
+            this.Controls.Add(this.titleButton);
+            this.Controls.Add(this.clearLabel);
+            this.Controls.Add(this.gameOverLabel);
+            this.Controls.Add(this.hiLabel);
+            this.Controls.Add(this.leftLabel);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.copyrightLabel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Form1";
+            this.Text = "よけとるVS22";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label copyrightLabel;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label leftLabel;
+        private System.Windows.Forms.Label hiLabel;
+        private System.Windows.Forms.Label gameOverLabel;
+        private System.Windows.Forms.Label clearLabel;
+        private System.Windows.Forms.Button titleButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label tempLabel;
+    }
+}
+
---
+++ yoketoruvs22/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace yoketoruvs22
+{
+    public partial class Form1 : Form
+    {
+        const bool isDebug = true;
+
+        const int SpeedMax = 20;
+        const int PlayerMax = 1;
+        const int EnemyMax = 110;
+        const int ItemMax = 1;
+        const int ChrMax = PlayerMax + EnemyMax + ItemMax;
+        Label[] chrs = new Label[ChrMax];
+        int[] vx = new int[ChrMax];
+        int[] vy = new int[ChrMax];
+        const int PlayerIndex = 0;
+        const int EnemyIndex = PlayerIndex + PlayerMax;
+        const int ItemIndex = EnemyIndex + EnemyMax;
+        const int StartTime = 120;
+
+        const string PlayerText = "👆";
+        const string EnemyText = "■";
+        const string ItemText = "◆";
+
+        static Random rand = new Random();
+
+        int itemCount;
+        int time;
+        int hiscore;
+
+        enum State
+        {
+            None = -1,  // 無効
+            Title,      // タイトル
+            Game,       // ゲーム
+            Gameover,   // ゲームオーバー
+            Clear       // クリア
+        }
+
+        State currentState = State.None;
+        State nextState = State.Title;
+
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(int vKey);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i=0;i<ChrMax;i++)
+            {
+                chrs[i] = new Label();
+                chrs[i].AutoSize = true;
+                if (i == PlayerIndex)
+                {
+                    chrs[i].Text = PlayerText;
+                }
+                else if (i < ItemIndex)
+                {
+                    chrs[i].Text = EnemyText;
+                }
+                else
+                {
+                    chrs[i].Text = ItemText;
+                }
+                chrs[i].Font = tempLabel.Font;
+                Controls.Add(chrs[i]);
+            }
+        }
+
+        private void titleButton_Click(object sender, EventArgs e)
+        {
+            nextState = State.Title;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (nextState != State.None)
+            {
+                initProc();
+            }
+
+            if (isDebug)
+            {
+                if (GetAsyncKeyState((int)Keys.O) < 0)
+                {
+                    nextState = State.Gameover;
+                }
+                else if (GetAsyncKeyState((int)Keys.C) < 0)
+                {
+                    nextState = State.Clear;
+                }
+            }
+
+            if (currentState == State.Game)
+            {
+                UpdateGame();
+            }
+        }
+
+        void UpdateGame()
+        {
+            time--;
+            timeLabel.Text = $"Time {time:000}";
+
+            Point mp = PointToClient(MousePosition);
+
+            // mpがラベルの中心になるようにする
+            chrs[PlayerIndex].Left = mp.X - chrs[PlayerIndex].Width /2;
+            chrs[PlayerIndex].Top = mp.Y - chrs[PlayerIndex].Height /2;
+
+            for (int i=EnemyIndex,j=ItemIndex;i< ChrMax;i++) // //
+            {
+                if (!chrs[i].Visible) continue;
+
+                chrs[i].Left += vx[i];
+                chrs[i].Top += vy[i];
+
+                if (chrs[i].Left < 0)
+                {
+                    vx[i] = Math.Abs(vx[i]);
+                }
+                if (chrs[i].Top < 0)
+                {
+                    vy[i] = Math.Abs(vy[i]);
+                }
+                if (chrs[i].Right > ClientSize.Width)
+                {
+                    vx[i] = -Math.Abs(vx[i]);
+                }
+                if (chrs[i].Bottom > ClientSize.Height)
+                {
+                    vy[i] = -Math.Abs(vy[i]);
+                }
+
+                if (chrs[j].Left < 0) ///*
+                {
+                    vx[j] = Math.Abs(vx[j]);
+                }
+                if (chrs[j].Top < 0)
+                {
+                    vy[j] = Math.Abs(vy[j]);
+                }
+                if (chrs[j].Right > ClientSize.Width)
+                {
+                    vx[j] = -Math.Abs(vx[j]);
+                }
+                if (chrs[j].Bottom > ClientSize.Height)
+                {
+                    vy[j] = -Math.Abs(vy[j]);
+                } //*/
+
+                if (    (mp.X >= chrs[i].Left)
+                    &&  (mp.X < chrs[i].Right)
+                    &&  (mp.Y >= chrs[i].Top)
+                    &&  (mp.Y < chrs[i].Bottom)) 
+                {
+                    // MessageBox.Show("重なった!!");
+                    if (i < ItemIndex)
+                    {
+                        //nextState = State.Gameover;
+                    }
+                    else
+                    {
+                        chrs[i].Visible = false;
+                        itemCount--;
+                        leftLabel.Text = $"◆：{itemCount:00}";
+                        if (itemCount <= 0)
+                        {
+                            nextState = State.Clear;
+                        }
+                    }
+                }
+
+                if (    (time <= 0)
+                    &&  (nextState == State.None))
+                {
+                    nextState = State.Gameover;
+                }
+            }
+        }
+
+        void initProc()
+        {
+            currentState = nextState;
+            nextState = State.None;
+
+            switch(currentState)
+            {
+                case State.Title:
+                    titleLabel.Visible = true;
+                    startButton.Visible = true;
+                    copyrightLabel.Visible = true;
+                    hiLabel.Visible = true;
+                    gameOverLabel.Visible = false;
+                    titleButton.Visible = false;
+                    clearLabel.Visible = false;
+                    break;
+
+                case State.Game:
+                    titleLabel.Visible = false;
+                    startButton.Visible = false;
+                    copyrightLabel.Visible = false;
+                    hiLabel.Visible = false;
+
+                    itemCount = ItemMax;
+                    time = StartTime + 1;
+                    leftLabel.Text = $"◆：{itemCount:00}";
+
+                    for (int i = EnemyIndex,j=ItemIndex; i < ChrMax; i++)
+                    {
+                        chrs[i].Left = rand.Next(ClientSize.Width - chrs[i].Width);
+                        chrs[i].Top = rand.Next(ClientSize.Height - chrs[i].Height);
+                        vx[i] = rand.Next(-SpeedMax, SpeedMax + 1);
+                        vy[i] = rand.Next(-SpeedMax, SpeedMax + 1);
+                        chrs[i].Visible = true;
+
+                        chrs[j].Left = rand.Next(ClientSize.Width - chrs[j].Width); ///*
+                        chrs[j].Top = rand.Next(ClientSize.Height - chrs[j].Height);
+                        vx[j] = SpeedMax-20;
+                        vy[j] = SpeedMax - 20;
+                        chrs[j].Visible = true; //*/
+                    }
+                    break;
+
+                case State.Gameover:
+                    gameOverLabel.Visible = true;
+                    titleButton.Visible = true;
+                    break;
+
+                case State.Clear:
+                    clearLabel.Visible = true;
+                    titleButton.Visible = true;
+                    hiLabel.Visible = true;
+                    if (time > hiscore)
+                    {
+                        //hiscore = time;
+                        //hiLabel.Text = $"HighScore {hiscore}";
+                    }
+                    break;
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            nextState = State.Game;
+        }
+    }
+}
